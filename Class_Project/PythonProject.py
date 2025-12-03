e4d0d5acabb374d2c1fb0c6682683ff63823f47f
Potato <mouapotato@gmail.com>
2025-12-03 04:06:24 -0700

    Add initial password generator program

--- Class_Project/PythonProject.py
+++ Class_Project/PythonProject.py
@@ -3,8 +3,5 @@ import secrets as s
 import tkinter as tk
 
 def generate_password():
-    length = int(length_entry.get())
-    alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+"
-    password = ''.join(s.choice(alphabet) for _ in range(length))
-    password_label.config(text=f"Generated Password: {password}")
+
 
