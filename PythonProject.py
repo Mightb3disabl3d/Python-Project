d584c2f9fad8786edbf6ae47691d4096969b6da9
Potato <mouapotato@gmail.com>
2025-12-03 03:56:20 -0700

    Auto-save commit

--- PythonProject.py
+++ PythonProject.py
@@ -1,1 +1,10 @@
 #This is a password generator program for a school project
+import secrets as s
+import tkinter as tk
+
+def generate_password():
+    length = int(length_entry.get())
+    alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+"
+    password = ''.join(s.choice(alphabet) for _ in range(length))
+    password_label.config(text=f"Generated Password: {password}")
+
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Python-Project
+# This is a repo for most python related projects from me. Probably will continue to work on more projects in the future.  
